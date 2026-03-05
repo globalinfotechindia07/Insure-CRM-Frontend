c2a0c620dca28a01e883058d809a2c368f38a141
globalinfotechindia07 <globalinfotechindia07@gmail.com>
2026-03-05 13:27:15 +0530

    Update api.js
    new path add

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // const REACT_APP_API_URL = 'https://miraicrm.com/api/';
 // const REACT_APP_API_URL = 'https://insure.isyncerp.com/api/';
-// const REACT_APP_API_URL = 'https://jpinsurancebroker.co.in/api/';
-const REACT_APP_API_URL = 'http://localhost:5050/api/';
+const REACT_APP_API_URL = 'https://jpinsurancebroker.co.in/api/';
+// const REACT_APP_API_URL = 'http://localhost:5050/api/';
 export default REACT_APP_API_URL;
 //  REACT_APP_API_URL;
 
